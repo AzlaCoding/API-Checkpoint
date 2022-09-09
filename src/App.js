04e86b6b8386f90f23f3f07316517e8f7b7a7b39
AzlaCoding <matabatawchihmk@gmail.com>
2022-09-09 01:04:49 +0100

    adding files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import UserList from "./components/UserList";
+
+function App() {
+  return (
+    <div className=" bg-base-100 my-6">
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
